fc92da227422358de13f6987dc6399ef30eb243b
pnastusevich <pasha.nastusevich@gmail.com>
2025-01-16 16:06:34 +0300

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Паша Настусевич on 16.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
