c1a3c420b1dab3972da35b981feaf50cf751dd8a
pnastusevich <pasha.nastusevich@gmail.com>
2025-01-16 22:37:40 +0300

    added display of cells. Customized the main screen design

---
+++ TodoList/Modules/TasksList/TaskCell/TaskCell.swift
@@ -0,0 +1,169 @@
+//
+//  TaskCell.swift
+//  TodoList
+//
+//  Created by Паша Настусевич on 16.01.25.
+//
+
+import UIKit
+
+protocol TaskCellDelegate: AnyObject {
+    func didTapCompleteButton(in cell: TaskCell)
+}
+
+protocol CellModelRepresentable {
+    var viewModel: TaskCellViewModelProtocol? { get }
+}
+
+final class TaskCell: UITableViewCell, CellModelRepresentable {
+    
+    static let cellID = "TaskCell"
+    weak var delegate: TaskCellDelegate?
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .gray
+        label.numberOfLines = 2
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+    
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private let isCompleteButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.contentVerticalAlignment = .fill
+        button.contentHorizontalAlignment = .fill
+        button.imageView?.contentMode = .scaleAspectFit
+        return button
+    }()
+    
+    private let separatorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.darkGray
+        return view
+    }()
+    
+    var viewModel: TaskCellViewModelProtocol? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupLayout()
+        isCompleteButton.addTarget(self, action: #selector(didTapCompleteButton), for: .touchUpInside)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+    @objc private func didTapCompleteButton() {
+        guard let viewModel = viewModel as? TaskCellViewModel else { return }
+        viewModel.isCompleted.toggle()
+        updateView()
+        
+        delegate?.didTapCompleteButton(in: self)
+    }
+    
+    func updateView() {
+        contentView.backgroundColor = .black
+        guard let viewModel = viewModel as? TaskCellViewModel else { return }
+        
+        timeLabel.text = viewModel.name
+        subtitleLabel.text = viewModel.subname
+        timeLabel.text = formatDateRange(date: viewModel.createdAt)
+        
+        if viewModel.isCompleted {
+            isCompleteButton.setImage(UIImage(systemName: "checkmark.circle"), for: .normal)
+            isCompleteButton.tintColor = .systemYellow
+            
+            let attributedString = NSAttributedString(string: viewModel.name, attributes: [
+                //                    .strikethroughStyle: NSUnderlineStyle.single.rawValue,
+                .foregroundColor: UIColor.gray,
+                .font: UIFont.boldSystemFont(ofSize: 18)
+            ]
+            )
+            titleLabel.attributedText = attributedString
+            subtitleLabel.textColor = .gray
+        } else {
+            isCompleteButton.setImage(UIImage(systemName: "circle"), for: .normal)
+            isCompleteButton.tintColor = .gray
+            
+            titleLabel.attributedText = nil
+            titleLabel.text = viewModel.name
+            titleLabel.textColor = .white
+            titleLabel.font = UIFont.boldSystemFont(ofSize: 18)
+            subtitleLabel.textColor = .white
+        }
+    }
+    
+    func formatDateRange(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        
+        let calendar = Calendar.current
+        if calendar.isDateInToday(date) {
+            dateFormatter.dateFormat = "'Сегодня, 'dd/MM/yy"
+        } else {
+            dateFormatter.dateFormat = "dd/MM/yy"
+        }
+        
+        let dateString = dateFormatter.string(from: date)
+        return "\(dateString)"
+    }
+
+    private func setupLayout() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(subtitleLabel)
+        contentView.addSubview(timeLabel)
+        contentView.addSubview(isCompleteButton)
+        contentView.addSubview(separatorView)
+        
+        NSLayoutConstraint.activate(
+            [
+                isCompleteButton.centerYAnchor.constraint(equalTo: titleLabel.centerYAnchor),
+                isCompleteButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+                isCompleteButton.widthAnchor.constraint(equalToConstant: 24),
+                isCompleteButton.heightAnchor.constraint(equalToConstant: 24),
+                
+                titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+                titleLabel.leadingAnchor.constraint(equalTo: isCompleteButton.trailingAnchor, constant: 16),
+                titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+                
+                subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+                subtitleLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+                subtitleLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+                
+                timeLabel.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 8),
+                timeLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+                timeLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+                
+                separatorView.topAnchor.constraint(equalTo: timeLabel.bottomAnchor, constant: 16),
+                separatorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+                separatorView.trailingAnchor.constraint(equalTo: timeLabel.trailingAnchor),
+                separatorView.heightAnchor.constraint(equalToConstant: 1),
+                separatorView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+        ]
+        )
+    }
+}
--- TodoList/Modules/TasksList/TaskListInteractor.swift
+++ TodoList/Modules/TasksList/TaskListInteractor.swift
@@ -9,10 +9,13 @@ import Foundation
 
 protocol TaskListInteractorInputProtocol {
     init(storageServices: StorageServicesProtocol, networkServices: NetworkServicesProtocol, presenter: TaskListInteractorOutputProtocol)
+    func fetchTaskList()
+    func doneTask(task: Task?)
+    func deleteTask(task: Task)
 }
 
 protocol TaskListInteractorOutputProtocol: AnyObject {
-    func taskListDidReceive()
+    func taskListDidReceive(with dataStore: TaskListDataStore)
 }
 
 
@@ -23,11 +26,67 @@ final class TaskListInteractor: TaskListInteractorInputProtocol {
     
     private unowned let presenter: TaskListInteractorOutputProtocol
     
+    private var isFetchingFromAPI = false
+    
     required init(storageServices: StorageServicesProtocol, networkServices: NetworkServicesProtocol, presenter: TaskListInteractorOutputProtocol) {
         self.storageServices = storageServices
         self.networkServices = networkServices
         self.presenter = presenter
     }
     
+    func fetchTaskList() {
+        storageServices.fetchDataTask { [weak self]
+            result in
+            switch result {
+            case .success(let taskList):
+                if taskList.isEmpty {
+                    if !self!.isFetchingFromAPI {
+                        self!.isFetchingFromAPI = true
+                        self!.fetchTasksFromAPI()
+                    }
+                } else {
+                    let dataStore = TaskListDataStore(tasksList: taskList)
+                    self?.presenter.taskListDidReceive(with: dataStore)
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
     
+    func fetchTasksFromAPI() {
+        networkServices.fetchData { [weak self] result in
+            switch result {
+            case .success(let taskList):
+                var newTasks: [Task] = []
+                let currentDate = Date()
+                
+                let group = DispatchGroup()
+                
+                for task in taskList.todos {
+                    group.enter()
+                    self?.storageServices.createTask(taskName: task.todo, description: "Нет описания у задачи, которая была получена из API", createdDate: currentDate, isCompleted: task.completed) { task in
+                        newTasks.append(task)
+                        group.leave()
+                    }
+                }
+                group.notify(queue: .main) {
+                    let dataStore = TaskListDataStore(tasksList: newTasks)
+                    self?.presenter.taskListDidReceive(with: dataStore)
+                }
+            case .failure(let error):
+                print("\(error.localizedDescription) ошибка в fetchTasksFromAPI")
+            }
+        }
+    }
+    
+    func doneTask(task: Task?) {
+        guard let task = task else { return }
+        storageServices.doneTask(task: task)
+        fetchTaskList()
+    }
+    func deleteTask(task: Task) {
+        storageServices.deleteTask(task: task)
+        fetchTaskList()
+    }
 }
